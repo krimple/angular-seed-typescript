abb2aaea5507e6b6749d94a415af9be5078b4dac
Ken Rimple <krimple@chariotsolutions.com>
2015-10-08 12:27:39 -0400

    first useful version

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var gls = require('gulp-live-server');
-var server = require('gulp-server-livereload');
+var tinylr;
+
 
 // in order for project-based gulp-typescript to work, you MUST create the project 
 // outside of a task...
@@ -25,6 +25,7 @@ gulp.task('setup', function(done) {
 gulp.task('assets', function() {
      gulp.src(['./src/**/*.json', './src/**/*.html', './src/**/*.css'])
             .pipe(gulp.dest('./web'));
+     
 });
 
 gulp.task('ts', function(done) {
@@ -40,7 +41,7 @@ gulp.task('ts', function(done) {
     return tsResult.js.pipe(gulp.dest('web/js'));
 });
 
-gulp.task('watch', ['watch.assets', 'watch.ts']);
+gulp.task('watch', ['watch.assets', 'watch.ts', 'watch.web']);
 
 gulp.task('watch.assets', ['assets'], function() {
    return gulp.watch(['./src/**/*.json', './src/**/*.html', './src/**/*.css'], ['assets']) 
@@ -50,15 +51,35 @@ gulp.task('watch.ts', ['ts'], function() {
     return gulp.watch('src/**/*.ts', ['ts']);
 });
 
-gulp.task('server', ['watch'], function() {
+// see https://community.nitrous.io/tutorials/setting-up-gulp-with-livereload-sass-and-other-tasks
+
+function notifyLiveReload(event) {
+    console.log('live reload on', event);
+    var fileName= require('path').relative(__dirname + '/web', event.path);
+    console.log('file name is', fileName);
+    tinylr.changed({
+        body: {
+            files: [fileName]
+        }
+    });
+}
+
+gulp.task('watch.web', function() {
+    gulp.watch('web/**', notifyLiveReload);
+});
 
 
+gulp.task('express', function() {
+    var express = require('express');
+    var app = express();
+    app.use(require('connect-livereload')({port:4002}));
+    app.use(express.static(__dirname + '/web'));
+    app.listen(4000, '0.0.0.0');
 });
-gulp.task('webserver', function() {
-  gulp.src('web/')
-    .pipe(server({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+
+gulp.task('livereload', function() {
+    tinylr = require('tiny-lr')();
+    tinylr.listen(35729);
 });
+
+gulp.task('default', ['express', 'livereload', 'watch']);
